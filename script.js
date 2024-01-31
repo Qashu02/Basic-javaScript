b5cf40a839b84568089051a2b55e1cba03fee5f0
Qashu02 <hafizqasim5566@gmail.com>
2024-01-31 02:25:22 -0800

    Arithmetic operation in js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("fisrt program in js");
+let a;
+a=5;
+let b=4;
+let c=a+b;
+console.log("a=",a)
+console.log("b=",b)
+console.log("a+b=",c);
+c=a*b;
+console.log("a*b=",c);
+c=a-b;
+console.log("a-b=",c);
+c=a/b;
+console.log("a/b=",c);
+c=a**b;
+console.log("a^b=",c);
+c=a%b;
+console.log("a mod b=",c);
