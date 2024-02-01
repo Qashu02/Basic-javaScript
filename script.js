c59875070970f353048358f796e932abd4856d41
Qashu02 <hafizqasim5566@gmail.com>
2024-02-01 01:40:50 -0800

    Logical operatior with Conditional Statements

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-//  Taking user input and checking number is multiple of 5 ?
+//  program to check number and giving grades 
 
+let score;
+score =prompt("Enter Your Marks in range (0--100)");
 
-// alert("hello");is only used for displaying message
-
-// prompt used for taking input from user
-
-
-let num=prompt("Enter a number");
-
-if(num === null) 
-{
-   alert("Please Reload and Enter a number ");
+if(score==null){
+    alert("reload and Enter your number first ");
 }
-
-// === is used for restriction that variable only complare with same data type
-else if (num%5===0){
-   alert ("Your number is multiple of 5");
+ else if( score <=100 && score>=80 ){
+     alert("Congratulations Your Grade A");
+     
+}
+ else if( score <=79 && score>=60 ){
+    alert("Congratulations Your Grade B");
 
 }
+ else if( score <=69 && score>=50 ){
+    alert("Congratulations Your Grade C");
+
+}
+else if( score <=59 && score>=40 ){
+    alert("Congratulations Your Grade D");
+
+}
+else if( score <=49 && score>=34 ){
+    alert("Congratulations Your Grade E ");
+
+}
+else if( score <=33 && score>=0 ){
+    alert("Your grade is F, Next Time Better :) ");
+
+}
+
 else{
-   alert( "Your number is not multiple of 5");
+    alert("Reload and Enter Marks in Range");
 }
