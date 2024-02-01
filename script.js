8617603d5386a597c78c5b710660930c0bbe0813
Qashu02 <hafizqasim5566@gmail.com>
2024-02-01 00:31:02 -0800

    Conditional Statement with Alert and prompt

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let a=2;
-a++;
-console.log(a);
-++a;
-console.log(a);
+//  Taking user input and checking number is multiple of 5 ?
+
+
+// alert("hello");is only used for displaying message
+
+// prompt used for taking input from user
+
+
+let num=prompt("Enter a number");
+
+if(num === null) 
+{
+   alert("Please Reload and Enter a number ");
+}
+
+// === is used for restriction that variable only complare with same data type
+else if (num%5===0){
+   alert ("Your number is multiple of 5");
+
+}
+else{
+   alert( "Your number is not multiple of 5");
+}
