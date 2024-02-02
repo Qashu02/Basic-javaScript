c4e0c9e93e6683159e829c9b98bde2922b272ad0
Qashu02 <hafizqasim5566@gmail.com>
2024-02-02 11:28:01 -0800

    string and string functions

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-//  program to check number and giving grades 
+// program that take username and create new username by concatinating with @ and length.
 
-let score;
-score =prompt("Enter Your Marks in range (0--100)");
 
-if(score==null){
-    alert("reload and Enter your number first ");
-}
- else if( score <=100 && score>=80 ){
-     alert("Congratulations Your Grade A");
-     
-}
- else if( score <=79 && score>=60 ){
-    alert("Congratulations Your Grade B");
+let name=prompt("Enter your Name");
+// concatination 
+let username= "@"+name.concat(name.length); 
 
-}
- else if( score <=69 && score>=50 ){
-    alert("Congratulations Your Grade C");
+// console.log(name.slice(2,3));
+// console.log(name.replace("s","z"));
+// console.log(name.trim)
+// name.charAt(0);
 
-}
-else if( score <=59 && score>=40 ){
-    alert("Congratulations Your Grade D");
 
-}
-else if( score <=49 && score>=34 ){
-    alert("Congratulations Your Grade E ");
 
-}
-else if( score <=33 && score>=0 ){
-    alert("Your grade is F, Next Time Better :) ");
-
-}
-
-else{
-    alert("Reload and Enter Marks in Range");
-}
+alert("Your username is "+username);
