8113f4c7f155c3edc58516ae84143cb7e3d621e1
Qashu02 <hafizqasim5566@gmail.com>
2024-02-03 01:59:16 -0800

    Arrays definied methods in js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-// program to change sales value in an arrays
+let arr= ["bloomberg", "google", "microsoft","oracle", "GPT"]
+// arr.shift for deleted first element 
+// unshift() for adding new element on first index 
 
-let items=[234,54,256,432,12,456];
 
-for(let i = 0 ; i<items.length ; i++){
-    let before =items[i];
-    let after=items[i]/10;
-    
-    items[i]=items[i]-after;
-    console.log(`The sales ${before} is 10% off ${items[i]}`);
-} 
+console.log("orignal", arr);
+let deleted= arr.shift(0);
+console.log ("deleted " , deleted);
+
+// replace add or remove we user splice()
+
+let replace= arr.splice(2,1,"uber");
+console.log("replace" ,arr);
+
+// push and pop used for adding and deleting elements from start 
+
+arr.push("java");
+// toString used for converting the array in Strings 
+console.log("Extended array:",  arr.toString());
