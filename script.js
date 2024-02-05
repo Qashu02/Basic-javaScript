b7e930e288c710a13520baf353cacdd9b4dc9923
Qashu02 <hafizqasim5566@gmail.com>
2024-02-05 02:33:15 -0800

    reduce method

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let arr=[23,33,40,89,99,93,92];
-// filter values and generate the output
-const maxScore=arr.filter( (val)=>{
-    return val>90;
+let n= prompt("Enter a number");
+let arr=[];
+for(let i =1; i<=n ;i++){
+     arr[i-1]=i;
+  
 }
-)
-console.log(maxScore);
+// reduce method use to take multiple input and generate single output like this
+
+let sum=arr.reduce((prev ,curr)=>{
+    return prev+curr;
+})
+let product=arr.reduce((prev,curr)=>{
+    return prev*curr;
+})
+console.log(arr);
+console.log(sum);
+console.log(product);
