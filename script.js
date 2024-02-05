591faf225531128b2acc238fb25c6b08afe992fa
Qashu02 <hafizqasim5566@gmail.com>
2024-02-05 02:19:29 -0800

    filter method

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// program that takes array and print its square 
-let arr=[1,42,2,3];
-// call back function are the function when we use function as a parameter or reutrn a new function 
-arr.forEach((val)=>{
-   let sqr=val*val;
-   console.log(`The suqare of ${val} is ${sqr}`);
-});
+let arr=[23,33,40,89,99,93,92];
+// filter values and generate the output
+const maxScore=arr.filter( (val)=>{
+    return val>90;
+}
+)
+console.log(maxScore);
